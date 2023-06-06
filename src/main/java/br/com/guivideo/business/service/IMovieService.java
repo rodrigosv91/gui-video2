9a9d2e886de3b2fd8b35faf119e384e8e9b3eaa1
rodrigosv91 <rodrigosv91@gmail.com>
2023-06-05 21:13:53 -0300

    edit workflow

--- src/main/java/br/com/guivideo/business/service/IMovieService.java
+++ src/main/java/br/com/guivideo/business/service/IMovieService.java
@@ -2,6 +2,12 @@ package br.com.guivideo.business.service;
 
 import br.com.guivideo.api.v1.dto.response.MovieResponse;
 
+import br.com.guivideo.business.entity.Movie;
+
+import java.util.List;
+
 public interface IMovieService {
     public MovieResponse registerMovie(String title);
+
+    public List<Movie> getMovies();
 }
